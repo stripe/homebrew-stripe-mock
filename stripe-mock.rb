e073f2b55006722827b903c4e8954e24c35ecdf4
goreleaserbot <goreleaser@carlosbecker.com>
2018-04-16 14:29:34 -0700

    Brew formula update for stripe-mock version 0.13.0

--- stripe-mock.rb
+++ stripe-mock.rb
@@ -1,9 +1,9 @@
 class StripeMock < Formula
   desc "stripe-mock is a mock HTTP server that responds like the real Stripe API. It can be used instead of Stripe's testmode to make test suites integrating with Stripe faster and less brittle."
   homepage "https://github.com/stripe/stripe-mock"
-  url "https://github.com/stripe/stripe-mock/releases/download/v0.12.0/stripe-mock_0.12.0_darwin_amd64.tar.gz"
-  version "0.12.0"
-  sha256 "82ea29429f827101350835f343b7aa18c07fb55f6339ac2bbed435bbbd7ba267"
+  url "https://github.com/stripe/stripe-mock/releases/download/0.13.0/stripe-mock_0.13.0_darwin_amd64.tar.gz"
+  version "0.13.0"
+  sha256 "b91b9ec8399a215dde078dfb2c40bbb63addfae14060896d741189d5082881b3"
 
   def install
     bin.install "stripe-mock"
