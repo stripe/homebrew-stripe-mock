84ada4af1bded71bff359e5f5845448abee55874
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-19 14:24:56 +0200

    stripe-mock version v0.34.0

--- stripe-mock.rb
+++ stripe-mock.rb
@@ -1,9 +1,9 @@
 class StripeMock < Formula
   desc "stripe-mock is a mock HTTP server that responds like the real Stripe API. It can be used instead of Stripe's testmode to make test suites integrating with Stripe faster and less brittle."
   homepage "https://github.com/stripe/stripe-mock"
-  url "https://github.com/stripe/stripe-mock/releases/download/v0.33.0/stripe-mock_0.33.0_darwin_amd64.tar.gz"
-  version "0.33.0"
-  sha256 "10dfc4fe592d150cc0eff887fb3c9a17e5c21886daed66e214f44db0c0918a7f"
+  url "https://github.com/stripe/stripe-mock/releases/download/v0.34.0/stripe-mock_0.34.0_darwin_amd64.tar.gz"
+  version "0.34.0"
+  sha256 "73268883bcd0a33daea507372f098444fe62810000e1e7b2f481b02cb6efad67"
 
   def install
     bin.install "stripe-mock"
@@ -11,7 +11,7 @@ class StripeMock < Formula
 
   plist_options :startup => false
 
-  def plist; <<~EOS
+  def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
@@ -42,6 +42,10 @@ class StripeMock < Formula
   </dict>
 </plist>
 
-  EOS
+    EOS
+  end
+
+  test do
+    
   end
 end
