0cbf6272197b13645ddd67bb78b7c65b75289215
stripe-ci <support@stripe.com>
2019-11-07 15:54:43 -0800

    Brew formula update for stripe-mock version v0.74.0

--- stripe-mock.rb
+++ stripe-mock.rb
@@ -2,16 +2,16 @@
 class StripeMock < Formula
   desc "stripe-mock is a mock HTTP server that responds like the real Stripe API. It can be used instead of Stripe's testmode to make test suites integrating with Stripe faster and less brittle."
   homepage "https://github.com/stripe/stripe-mock"
-  version "0.73.3"
+  version "0.74.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/stripe/stripe-mock/releases/download/v0.73.3/stripe-mock_0.73.3_darwin_amd64.tar.gz"
-    sha256 "0ce6dadf0a54e1029b4c2b35cfa88038b5247aef5621c688aa0cda8af7562ef5"
+    url "https://github.com/stripe/stripe-mock/releases/download/v0.74.0/stripe-mock_0.74.0_darwin_amd64.tar.gz"
+    sha256 "3f60891c781d0cb2fc966dadbd1a8acc9e5477826fccc887bdcfe599c578be4f"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/stripe/stripe-mock/releases/download/v0.73.3/stripe-mock_0.73.3_linux_amd64.tar.gz"
-      sha256 "53e53516e272e018734a0758251bd39f9393adc14f5d11d759370d3a3c6220db"
+      url "https://github.com/stripe/stripe-mock/releases/download/v0.74.0/stripe-mock_0.74.0_linux_amd64.tar.gz"
+      sha256 "4995a5121ef96f990023d20ecef69fcea00afdaa7ce723c9a328e28e3a3d1560"
     end
   end
 
